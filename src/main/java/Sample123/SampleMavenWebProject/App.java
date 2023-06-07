0509eaa5c548b7aa829b7d1201030b5a52f6b16f
navinmadgundi288 <navinmadgundi@gmail.com>
2023-06-07 15:13:07 +0530

    Initial Commit

---
+++ src/main/java/Sample123/SampleMavenWebProject/App.java
@@ -0,0 +1,13 @@
+package Sample123.SampleMavenWebProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
